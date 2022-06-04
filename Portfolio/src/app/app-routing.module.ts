7de659293d9a9ec123642ef137f0924967582347
TereTorres <teretorres@outlook.com.ar>
2022-06-04 11:45:05 -0300

    rutas

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: AppComponent},
+  {path:'SobreMi',component: SobreMiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
